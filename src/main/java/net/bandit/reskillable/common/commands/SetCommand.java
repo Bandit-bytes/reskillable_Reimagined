c9302780bc539b477547717c78095472a20ee675
bandit-bytes <banditheeler2020@gmail.com>
2025-05-22 22:59:06 -0400

    fix set command to allow to other players

--- src/main/java/net/bandit/reskillable/common/commands/SetCommand.java
+++ src/main/java/net/bandit/reskillable/common/commands/SetCommand.java
@@ -6,10 +6,11 @@ import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.bandit.reskillable.Configuration;
 import net.bandit.reskillable.common.capabilities.SkillModel;
-import net.bandit.reskillable.common.network.SyncToClient;
 import net.bandit.reskillable.common.commands.skills.Skill;
+import net.bandit.reskillable.common.network.SyncToClient;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.event.RegisterCommandsEvent;
@@ -20,78 +21,55 @@ import net.minecraftforge.server.command.EnumArgument;
 @Mod.EventBusSubscriber
 public class SetCommand {
 
-    /**
-     * Registers the skill commands and returns the LiteralArgumentBuilder.
-     */
     public static LiteralArgumentBuilder<CommandSourceStack> register() {
         return Commands.literal("skills")
+                .requires(source -> source.hasPermission(2))
                 .then(Commands.literal("add")
-                        .then(Commands.argument("skill", EnumArgument.enumArgument(Skill.class))
-                                .then(Commands.argument("amount", IntegerArgumentType.integer(1))
-                                        .executes(SetCommand::executeAdd))))
+                        .then(Commands.argument("player", EntityArgument.player())
+                                .then(Commands.argument("skill", EnumArgument.enumArgument(Skill.class))
+                                        .then(Commands.argument("amount", IntegerArgumentType.integer(1))
+                                                .executes(SetCommand::executeAdd)))))
                 .then(Commands.literal("set")
-                        .then(Commands.argument("skill", EnumArgument.enumArgument(Skill.class))
-                                .then(Commands.argument("level", IntegerArgumentType.integer(1, Configuration.getMaxLevel()))
-                                        .executes(SetCommand::executeSet))));
+                        .then(Commands.argument("player", EntityArgument.player())
+                                .then(Commands.argument("skill", EnumArgument.enumArgument(Skill.class))
+                                        .then(Commands.argument("level", IntegerArgumentType.integer(1, Configuration.getMaxLevel()))
+                                                .executes(SetCommand::executeSet)))));
     }
 
-    /**
-     * Executes the add command.
-     *
-     * @param context The command context.
-     * @return A success code.
-     * @throws CommandSyntaxException If there is an error in the command syntax.
-     */
     private static int executeAdd(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
-        CommandSourceStack source = context.getSource();
-
-        if (!(source.getEntity() instanceof ServerPlayer player)) {
-            source.sendFailure(Component.literal("This command can only be used by a player."));
-            return 0;
-        }
-
+        ServerPlayer player = EntityArgument.getPlayer(context, "player");
         Skill skill = context.getArgument("skill", Skill.class);
         int amount = IntegerArgumentType.getInteger(context, "amount");
 
-        SkillModel skillModel = SkillModel.get(player);
-        int currentLevel = skillModel.getSkillLevel(skill);
-        int newLevel = Math.min(currentLevel + amount, Configuration.getMaxLevel());
-
-        skillModel.setSkillLevel(skill, newLevel);
+        SkillModel model = SkillModel.get(player);
+        int current = model.getSkillLevel(skill);
+        int newLevel = Math.min(current + amount, Configuration.getMaxLevel());
+        model.setSkillLevel(skill, newLevel);
         SyncToClient.send(player);
 
-        if (Configuration.isSkillUpMessageEnabled()) {
-            source.sendSuccess(() -> Component.translatable(skill.displayName)
-                    .append(" increased by " + amount + " to " + newLevel), true);
-        }
+        context.getSource().sendSuccess(() -> Component.literal("Increased ")
+                .append(player.getName())
+                .append("'s ")
+                .append(Component.literal(skill.name()))
+                .append(" by " + amount + " to " + newLevel), true);
 
         return 1;
     }
 
-    /**
-     * Executes the set command.
-     *
-     * @param context The command context.
-     * @return A success code.
-     * @throws CommandSyntaxException If there is an error in the command syntax.
-     */
     private static int executeSet(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
-        CommandSourceStack source = context.getSource();
-
-        if (!(source.getEntity() instanceof ServerPlayer player)) {
-            source.sendFailure(Component.literal("This command can only be used by a player."));
-            return 0;
-        }
-
+        ServerPlayer player = EntityArgument.getPlayer(context, "player");
         Skill skill = context.getArgument("skill", Skill.class);
         int level = IntegerArgumentType.getInteger(context, "level");
 
-        SkillModel skillModel = SkillModel.get(player);
-        skillModel.setSkillLevel(skill, level);
+        SkillModel model = SkillModel.get(player);
+        model.setSkillLevel(skill, level);
         SyncToClient.send(player);
 
-        source.sendSuccess(() -> Component.translatable(skill.displayName)
-                .append(" set to " + level), true);
+        context.getSource().sendSuccess(() -> Component.literal("Set ")
+                .append(player.getName())
+                .append("'s ")
+                .append(skill.displayName)
+                .append(" to " + level), true);
 
         return 1;
     }
